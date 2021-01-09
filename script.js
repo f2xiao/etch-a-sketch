7dec2c40be244f62f3d5aa02d7846b7d88467d7f
fangfang xiao <fxiao.mecheng@gmail.com>
2021-01-09 01:11:31 -0500

    Add button features

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const gridContainer = document.querySelector(".grid-container");
-const slider = document.querySelector(".slider");
-const gridSize = document.querySelector("#grid-size");
-let cells;
-let gridNumber = slider.value;
-gridSize.textContent = gridNumber;
-
-slider.addEventListener("change", function (e) {
-  gridNumber = e.target.value;
-  gridSize.textContent = gridNumber;
-  deleteBoard();
-  createBoard(gridNumber);
-});
-
 const deleteBoard = function () {
   cells.forEach((cell) => {
     cell.remove();
   });
 };
 
-const createBoard = function (gridNumber) {
+const eraser = function () {
+  cells = gridContainer.querySelectorAll("div");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", function (e) {
+      e.target.classList.add(`cell-white`);
+    });
+  });
+};
+
+const createBoard = function (gridNumber, color) {
   for (let i = 0; i < gridNumber; i++) {
     for (let j = 0; j < gridNumber; j++) {
       gridContainer.appendChild(document.createElement("div"));
@@ -29,9 +24,39 @@ const createBoard = function (gridNumber) {
   cells.forEach((cell) => {
     cell.classList.add("cell");
     cell.addEventListener("mouseover", function (e) {
-      e.target.classList.add("cell-active");
+      e.target.classList.add(`cell-${color}`);
     });
   });
 };
 
-createBoard(gridNumber);
+const gridContainer = document.querySelector(".grid-container");
+const slider = document.querySelector(".slider");
+const gridSize = document.querySelector("#grid-size");
+const clearButton = document.getElementById("clear");
+const blackButton = document.getElementById("black");
+const eraserButton = document.getElementById("eraser");
+let cells;
+let gridNumber = slider.value;
+gridSize.textContent = gridNumber;
+
+createBoard(gridNumber, "black");
+
+clearButton.addEventListener("click", function () {
+  cells.forEach((cell) => {
+    cell.classList.add("cell-white");
+  });
+});
+
+slider.addEventListener("change", function (e) {
+  gridNumber = e.target.value;
+  gridSize.textContent = gridNumber;
+  deleteBoard();
+  createBoard(gridNumber, "black");
+});
+
+blackButton.addEventListener("click", function () {
+  deleteBoard();
+  createBoard(gridNumber, "black");
+});
+
+eraserButton.addEventListener("click", eraser);
